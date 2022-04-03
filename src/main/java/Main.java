5f5574239cd2c85f34db9d48f4799bc91a1e86b9
Patcharawat <patcharawat.d@gmail.com>
2022-04-03 16:40:10 +0700

    imp exercise

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,61 +30,13 @@ public class Main {
     // Declarative approach ✅
 
     // Filter
-    List<Person> females = people.stream()
-        .filter(person -> person.getGender().equals(Gender.FEMALE))
-        .collect(Collectors.toList());
-
-//    females.forEach(System.out::println);
-
     // Sort
-    List<Person> sorted = people.stream()
-        .sorted(Comparator.comparing(Person::getAge).thenComparing(Person::getGender).reversed())
-        .collect(Collectors.toList());
-
-//    sorted.forEach(System.out::println);
-
     // All match
-    boolean allMatch = people.stream()
-        .allMatch(person -> person.getAge() > 8);
-
-//    System.out.println(allMatch);
     // Any match
-    boolean anyMatch = people.stream()
-        .anyMatch(person -> person.getAge() > 121);
-
-//    System.out.println(anyMatch);
     // None match
-    boolean noneMatch = people.stream()
-        .noneMatch(person -> person.getName().equals("Antonio"));
-
-//    System.out.println(noneMatch);
-
     // Max
-    people.stream()
-        .max(Comparator.comparing(Person::getAge));
-//        .ifPresent(System.out::println);
-
     // Min
-    people.stream()
-        .min(Comparator.comparing(Person::getAge));
-//        .ifPresent(System.out::println);
-
     // Group
-    Map<Gender, List<Person>> groupByGender = people.stream()
-        .collect(Collectors.groupingBy(Person::getGender));
-
-//    groupByGender.forEach((gender, people1) -> {
-//      System.out.println(gender);
-//      people1.forEach(System.out::println);
-//      System.out.println();
-//    });
-
-    Optional<String> oldestFemaleAge = people.stream()
-        .filter(person -> person.getGender().equals(Gender.FEMALE))
-        .max(Comparator.comparing(Person::getAge))
-        .map(Person::getName);
-
-    oldestFemaleAge.ifPresent(System.out::println);
   }
 
   private static List<Person> getPeople() {
